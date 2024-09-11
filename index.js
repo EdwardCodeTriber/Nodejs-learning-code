3f8323aa2dbfa972d5db35c8367f056885f7aa30
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-11 10:12:57 +0200

    Added new modules

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-// Commonjs , used to import node modules and executes them in order
-// the export value does not have to be the same as the return value
-// It reads as a default export value
-const add = require('./add')
-console.log("this one")
-
-const sum = add(4, 9)
-const sub = add(-4,9)
-console.log(sum)
-console.log(sub)
+ require('./batman')
+ require('./superman')
