6fb3eb8219aa92c59572b171520314bb5a3d8342
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-11 15:18:00 +0200

    more than one function. pattern 2

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const add = require('./math');
+const math = require('./math');
 // works the same way
+// Distructuring the object in math.js file
+const {add, subtract} = math;
 console.log(add(2, 7));
+console.log(subtract(2, 7));
 
 
--- math.js
+++ math.js
@@ -6,8 +6,22 @@
 // module.exports = add;
 
 // new way or better way 
-module.exports = (a,b) =>{
-    return a + b;
+// module.exports = (a,b) =>{
+//     return a + b;
+// }
+
+/// more than one function
+module.export.add = (a, b) =>{
+    return a+ b;
 }
 
- 
+module.exports = add;
+
+module.export.subtract = (a,b) =>{
+    return a-b;
+}
+
+module.exports = {
+     add,
+    subtract,
+}
