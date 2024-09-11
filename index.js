19d2c9e06886b3707c1e7c4739b07345cdce54c2
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-11 11:20:51 +0200

    Added a module caching method

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
- require('./batman')
- require('./superman')
+const superHero = require('./super-hero')
+
+console.log(superHero.getName());
+
+superHero.setName("superman");
+
+console.log(superHero.getName());
+
+// Module Caching
+const newSuperHero = require('./super-hero')
+console.log(newSuperHero.getName())
+
---
+++ super-hero.js
@@ -0,0 +1,15 @@
+//Module Caching
+
+class SuperHero{
+    constructor(name) {
+        this.name = name;
+    }
+    getName(){
+        return this.name;
+    }
+    setName(name){
+        this.name = name;
+    }
+}
+
+module.exports = new SuperHero('Batman')
