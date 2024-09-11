9fc55c9509d23216bb4a8b39cf5f9f170ea688bc
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-11 10:02:35 +0200

    Added a module exports to make the code of one module to be reusable in other module that need the function/s

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Commonjs , used to import node modules and executes them in order
-require('./add')
+const add = require('./add')
 console.log("this one")
+
+const sum = add(4, 9)
+
+console.log(sum)
